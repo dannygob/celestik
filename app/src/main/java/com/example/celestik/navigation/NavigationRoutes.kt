ce83e355f562a4977331a5342dfdcbfba67d9f31
Superhola <68669782+dannygob@users.noreply.github.com>
2025-11-07 14:10:51 +0100

    Add Login, Settings, and DetectionList routes
    add route

--- app/src/main/java/com/example/celestik/navigation/NavigationRoutes.kt
+++ app/src/main/java/com/example/celestik/navigation/NavigationRoutes.kt
@@ -1,14 +1,24 @@
 package com.example.celestik.navigation
 
-
 sealed class NavigationRoutes(val route: String) {
+
+    object Login : NavigationRoutes("login")
+
     object Dashboard : NavigationRoutes("dashboard")
+
     object Camera : NavigationRoutes("camera")
+
     object Details : NavigationRoutes("details/{detailType}") {
         fun createRoute(detailType: String) = "details/$detailType"
     }
 
     object Calibration : NavigationRoutes("calibration")
+
     object ReportDialog : NavigationRoutes("report_dialog")
+
     object Preview : NavigationRoutes("inspection_preview")
+
+    object Settings : NavigationRoutes("settings")
+
+    object DetectionList : NavigationRoutes("detection_list")
 }
