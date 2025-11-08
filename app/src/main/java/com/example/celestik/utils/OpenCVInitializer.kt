ee07f47399c211dd70ed51aa6e56ed63befc8163
Superhola <68669782+dannygob@users.noreply.github.com>
2025-11-08 12:35:38 +0100

    Update OpenCVInitializer.kt

--- app/src/main/java/com/example/celestik/utils/OpenCVInitializer.kt
+++ app/src/main/java/com/example/celestik/utils/OpenCVInitializer.kt
@@ -4,13 +4,17 @@ import android.content.Context
 import android.util.Log
 import org.opencv.android.OpenCVLoader
 
+/**
+ * Initializes OpenCV using debug mode and logs the result.
+ * Should be called once during app startup or camera activation.
+ */
 object OpenCVInitializer {
 
     fun initOpenCV(context: Context) {
         if (OpenCVLoader.initDebug()) {
-            Log.d("OpenCV", "OpenCV initialized successfully")
+            Log.d("OpenCV", "OpenCV initialized successfully") // TODO: Localize
         } else {
-            Log.e("OpenCV", "OpenCV initialization failed")
+            Log.e("OpenCV", "OpenCV initialization failed") // TODO: Localize
         }
     }
 }
