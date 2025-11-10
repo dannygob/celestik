e0ed9579b6da8078bf233553dd28b82ba0d65131
Superhola <dannygomeb@gmail.com>
2025-11-10 18:28:05 +0100

    update tensor flow
    Signed-off-by: Superhola <dannygomeb@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.ui.text)
+    implementation(libs.protolite.well.known.types)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
