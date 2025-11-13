a42369ed39fe5c2043bf01afc35d2f4ca16e9335
Superhola <68669782+dannygob@users.noreply.github.com>
2025-11-13 07:35:42 +0100

    Update ImageUtils.kt

--- app/src/main/java/com/example/celestik/utils/ImageUtils.kt
+++ app/src/main/java/com/example/celestik/utils/ImageUtils.kt
@@ -1,13 +1,17 @@
 package com.example.celestik.utils
 
+import android.graphics.Bitmap
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.core.graphics.createBitmap
 import org.opencv.android.Utils
 import org.opencv.core.Mat
-import androidx.core.graphics.createBitmap
 
 fun matToImageBitmap(mat: Mat): ImageBitmap {
-    val bitmap = createBitmap(mat.cols(), mat.rows())
+    if (mat.empty() || mat.cols() <= 0 || mat.rows() <= 0) {
+        throw IllegalArgumentException("El Mat proporcionado está vacío o tiene dimensiones inválidas.")
+    }
+    val bitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888)
     Utils.matToBitmap(mat, bitmap)
     return bitmap.asImageBitmap()
 }
